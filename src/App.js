b1b33e450e6e392280dcbf390625df37f887aa49
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-04-28 11:55:25 -0500

    Added rel="noreferrer" on target blank

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/asthermoreno10/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/asthermoreno10/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Ash Moreno
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/AshM10/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/AshM10/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on Github
         </a>
         .
