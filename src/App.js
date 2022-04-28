90f4146a0b17a6dc3b089d826bf4b09fbc007fac
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-04-28 15:57:22 -0500

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/asthermoreno10/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Ash Moreno
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/AshM10/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/asthermoreno10/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ash Moreno
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AshM10/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
