e80913321934390306b6c215a5505d56ff66eadd
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-04-28 11:53:45 -0500

    Added the Github repository URL.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/asthermoreno10/" target="_blank">
+          Ash Moreno
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/AshM10/react-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
