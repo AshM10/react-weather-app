5ed9ff92148bb2ed6d06062b9828cf16a79d595a
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-05-06 16:39:33 -0500

    Finalized the styling.

--- src/Weather.js
+++ src/Weather.js
@@ -63,24 +63,23 @@ export default function Weather(props) {
         <hr />
         <div className="WeatherInfo">
           <h1>{weatherData.city}</h1>
-          <ul>
-            <li>
-              <FormattedDate date={weatherData.date} />
-            </li>
-            <li className="text-capitalize">{weatherData.description}</li>
-          </ul>
           <div className="row mt-3">
-            <div className="col-2">
-              <WeatherIcon code={weatherData.iconUrl} size={60} />
-            </div>
             <div className="col-6">
-              <WeatherTemperature celsius={weatherData.temperature} />
-            </div>
-            <div className="col-4">
               <ul>
+                <li>
+                  {" "}
+                  <FormattedDate date={weatherData.date} />
+                </li>
+                <li className="text-capitalize">{weatherData.description}</li>
                 <li>Humidity: {weatherData.humidity}%</li>
                 <li>Wind: {weatherData.wind} km/h</li>
               </ul>
+            </div>
+            <div className="col-2">
+              <WeatherIcon code={weatherData.iconUrl} size={60} />
+            </div>
+            <div className="col-4">
+              <WeatherTemperature celsius={weatherData.temperature} />
             </div>
           </div>
         </div>
